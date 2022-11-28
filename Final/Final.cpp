f000f47c405db4cc6047d9e3900c76f5c5e6b941
Abhishek Parekh <abhishekparekh25@yahoo.ca>
2022-11-28 11:49:15 -0500

    Finalize class, first version, will test and debug today

---
+++ Final/Final.cpp
@@ -0,0 +1,145 @@
+//The Final class contains the main fuctions for combining the results from the Reduce class, and merging them into one output file
+
+#include "Final.h"
+#include "FileIO.h"
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <Windows.h>
+#include <filesystem>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+using namespace std; 
+namespace fs = std::filesystem;
+
+
+//setting buffer
+Final::Final()
+    :   bufferLimit(1024) {
+    }
+
+//allocating buffer memory
+Final::Final(std::string FinalDir)
+    :   bufferLimit(1024),  finalOutputDirectory(FinalDir)
+    {}
+
+Final::Final(const string tMemory, size_t size_of_buffer)
+    : intermediateDirectory{tMemory }, bufferLimit{ size_of_buffer }
+    {}
+
+//destructor
+Final::~Final() 
+    {};
+
+
+/*Sets up the FinalOutput Directory, where the a single merged output file and a success file will written to*/
+void Final::setUpFinalDirectory(std::string FinalDirIn)
+{
+    finalOutputDirectory = FinalDirIn;
+}
+
+
+/*Function getFileList is used to verify if any files exist in the Reducer Directory
+if there are files, the bool listOfFiles will be set to true, else false*/
+bool Final::getFileList(const std::string& inputFolder, std::vector<std::string>& fileList) {
+    bool listOfFiles = true;
+    
+    if (IO_MAN.getListOfTextFiles(inputFolder, fileList)) {
+        bool listOfFiles = true;
+    }
+
+    else {
+        cout << "No files exist in the Reducer Directory" << endl;
+        bool listOfFiles = false;
+    }
+
+    return listOfFiles;
+}
+
+
+/*the function importFromReduce is used for merging data from multiple files into one, the data is obtained from the
+Reducer Directory, and imported into the Final
+*/
+bool Final::mergeFromReduce(const std::string& folderPath, const std::string& fileName, std::string& finalOutputVec)
+{
+    //import data from multiple files into a vector
+    //have a while loop that says if any files are present in output directory then continue to process
+    //if not then cout << No more files remain for extraction << endl;
+    finalOutputVec = finalOutputFileName; 
+
+    //empty vector declared
+    std::vector<std::string> finVec;
+    bool dataImportedSuccess = true; 
+    bool listOfFiles;
+
+    if (listOfFiles = true) {
+            
+        //read contents from file into finVec
+        if (IO_MAN.readFileIntoVector(folderPath, fileName, finVec))
+            {
+                for (size_t i = 0; i < finVec.size(); i++) {
+                    
+                    if (listOfFiles = false) {
+                        cout << "ERROR: Merge Unsuccessful at line " << finVec.at(i) << endl;
+                    }
+                } 
+                //exSuccess(); //print out Success after merge is complete
+            }
+
+        else {
+            cout << "ERROR: Issue with transfering data from reducer to Finalier" << endl;
+            dataImportedSuccess = false; //not successful in transfering data
+        }
+
+        //exSuccess();
+    }
+
+    exSuccess(); //print out Success after merge is complete
+    return dataImportedSuccess;
+}    
+        
+
+/*
+void Final::finalFile(const string& folderpath, const string& filename, std::string& finalOutputFile) 
+{
+}
+*/
+
+/*export a single output file with all data merged*/
+bool Final::exportFinal(const std::string& fileName, std::vector<std::string> finVec)
+{
+     IO_MAN.writeVectorToFile(finalOutputDirectory, fileName, finVec, true);
+     return true;
+}
+
+bool Final::finalFile(const std::string line) {
+
+    bool dataImportedSuccess;
+    std::vector<string> finVec;
+    if (dataImportedSuccess = true) {
+        
+        exportFinal(finalOutputFileName, finVec);
+
+    }
+
+    else {
+
+        cout << "ERROR: failure to package and export the merged file" << endl;
+    }
+}
+
+
+/*export an output file with success written in it.*/
+void Final::exSuccess()
+{
+    //writing success to an output file SUCCESS.TXT
+    //After the completion of joining the results in finalFile
+
+        std::string fName = "SUCCESS.txt";
+        std::vector<std::string> vSucc;
+        vSucc.push_back("SUCESSS!");
+        IO_MAN.writeVectorToFile(finalOutputDirectory, fName, vSucc);
+        return;
+}
---
+++ Final/Final.h
@@ -0,0 +1,69 @@
+//header file for FINAL CLASS
+
+#pragma once
+#ifndef FINAL_H
+#define FINAL_H
+
+#include <string>
+#include <vector>
+#include <map>
+using std::string;
+using std::vector;
+#include <iostream>
+#include <ostream>
+#include "FileIO.h"
+
+typedef std::pair<string, int> finVec;
+//typedef std::vector<string> listOfFiles;
+
+class Final {
+    public:
+
+        //constructor
+        Final();
+
+        //final directory will take in type string
+        Final(std::string FinalDir);
+
+        //intermediate memory 
+        Final(const string tMemory, size_t size_of_buffer);
+
+        //destructor
+        ~Final();
+
+        //setting up final directory
+        void setUpFinalDirectory(std::string FinalDirIn);
+
+        //function that attaches the merge and export
+        //void finalFile(const string& folderpath, const string& filename, std::string& finalOutputFile);
+
+        //export final output file
+        //void exportFinalResults();
+
+    protected:
+
+        //bring in data from reduced files
+        bool mergeFromReduce(const std::string& folderPath, const std::string& fileName, std::string& finalVec);
+
+        bool getFileList(const std::string& inputFolder, std::vector<std::string>& fileList);
+
+        //join all data items into one vector, adding lines/phrase to map
+        //void mergeContent(const string& folderpath, const string& filename);
+
+        bool Final::exportFinal(const std::string& fileName, std::vector<std::string> finVec);
+
+        bool finalFile(const std::string line);
+
+        //export a file with success written in it.
+        void exSuccess();
+
+    private:
+        std::string finalOutputDirectory;
+        size_t bufferLimit;
+        string intermediateDirectory;
+        FileIOManagement IO_MAN; //using fileIO class
+        const std::string finalOutputFileName = "FinalOutput.txt";
+        //std::map<std::string, uint32_t> reducedItems;
+};
+
+#endif //FINAL_H
